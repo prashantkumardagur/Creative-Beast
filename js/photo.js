17c7445c80fbdd33e00673ee2202bad4d2379218
PRASHANT KUMAR <prashantkumardagur@gmail.com>
2020-04-04 18:34:10 +0530

    abc

--- js/photo.js
+++ js/photo.js
@@ -4,5 +4,5 @@ var contain = document.getElementById("container");
 
 function view(x){
     contain.style.display="block";
-    main.src=images[x].src;
+    main.src=(images[x].src).slice(0,-5) + "m-min.jpeg";
 }
